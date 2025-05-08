b268b47ae7672fa76dfa862c55800f1b949143fe
ecastillov <95753248+ecastillov@users.noreply.github.com>
2025-05-08 10:50:04 +0200

    Update Program.cs

--- Labfiles/02-qna/C-Sharp/qna-app/Program.cs
+++ Labfiles/02-qna/C-Sharp/qna-app/Program.cs
@@ -3,7 +3,9 @@ using System.IO;
 using System.Text;
 using Microsoft.Extensions.Configuration;
 
-// Import namespaces
+// import namespaces
+using Azure;
+using Azure.AI.Language.QuestionAnswering;
 
 
 namespace qna_app
@@ -22,10 +24,31 @@ namespace qna_app
                 string projectName = configuration["QAProjectName"];
                 string deploymentName = configuration["QADeploymentName"];
 
-                // Create client using endpoint and key
+               // Create client using endpoint and key
+                AzureKeyCredential credentials = new AzureKeyCredential(aiSvcKey);
+                Uri endpoint = new Uri(aiSvcEndpoint);
+                QuestionAnsweringClient aiClient = new QuestionAnsweringClient(endpoint, credentials);
+
 
 
                 // Submit a question and display the answer
+                string user_question = "";
+                while (true)
+                    {
+                        Console.Write("Question: ");
+                        user_question = Console.ReadLine();
+                        if (user_question.ToLower() == "quit")
+                            break;
+                        QuestionAnsweringProject project = new QuestionAnsweringProject(projectName, deploymentName);
+                        Response<AnswersResult> response = aiClient.GetAnswers(user_question, project);
+                        foreach (KnowledgeBaseAnswer answer in response.Value.Answers)
+                        {
+                            Console.WriteLine(answer.Answer);
+                            Console.WriteLine($"Confidence: {answer.Confidence:P2}");
+                            Console.WriteLine($"Source: {answer.Source}");
+                            Console.WriteLine();
+                        }
+                    }
                 
 
             }
